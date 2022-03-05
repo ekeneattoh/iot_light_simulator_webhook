b132f1c579a08909918460439ef92dd6cd6afab1
ekeneattoh <ekeneattoh@gmail.com>
2022-03-05 20:56:37 +0100

    cleanup, new function

--- smart_thermostat/__init__.py
+++ smart_thermostat/__init__.py
@@ -6,25 +6,30 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    temp = req.params.get('temp')
-    if not temp:
+    command = req.params.get('command')
+    if not command:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            temp = req_body.get('temp')
+            command = req_body.get('command')
 
-    if temp >= 1:
+    if command == "on":
         return func.HttpResponse(
             json.dumps({
-                "temperature": temp,
+                "data": "AC is ON",
                 "environment": "Home"
 
             })
         )
+    elif command == "off":
+        return func.HttpResponse(json.dumps({
+            "data": "AC is OFF",
+            "environment": "Home"
+        }))
     else:
         return func.HttpResponse(
-            "Please pass a valid temp above 0 on the query string or in the request body",
+            "Please pass a valid command on/off on the query string or in the request body",
             status_code=400
         )
