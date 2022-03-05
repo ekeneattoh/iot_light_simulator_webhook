aae28614c4fa4000ee24c9a3354686772ceac4e2
ekeneattoh <ekeneattoh@gmail.com>
2022-03-05 20:28:49 +0100

    added fake thermostat

---
+++ smart_thermostat/__init__.py
@@ -0,0 +1,30 @@
+import logging
+import json
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    temp = req.params.get('temp')
+    if not temp:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            temp = req_body.get('temp')
+
+    if temp >= 1:
+        return func.HttpResponse(
+            json.dumps({
+                "temperature": temp,
+                "environment": "Home"
+
+            })
+        )
+    else:
+        return func.HttpResponse(
+            "Please pass a valid temp above 0 on the query string or in the request body",
+            status_code=400
+        )
